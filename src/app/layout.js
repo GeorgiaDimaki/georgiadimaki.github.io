f55c05ece9e7a10f450bffcbe0c38cb02d715eef
Georgia Dimaki <ggdimaki@gmail.com>
2025-09-21 19:26:36 -0400

    feat: Add Google Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// import { Lora, Source_Sans_3 } from 'next/font/google';
+import Script from 'next/script';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,9 +19,22 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  gaMeasurementId = 'G-B62LXGZ6QE';
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gaMeasurementId}');
+          `}
+        </Script>
         {children}
       </body>
     </html>
